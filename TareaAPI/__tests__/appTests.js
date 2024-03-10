ddc4f422b5b89730f10a695f610a505a49c1321e
javialroro <102406440+javialroro@users.noreply.github.com>
2024-03-09 18:38:45 -0600

    l;kasdflk

--- TareaAPI/__tests__/appTests.js
+++ TareaAPI/__tests__/appTests.js
@@ -1,6 +1,12 @@
 // app.test.js
 const request = require('supertest');
 const app = require('../app');
+const jwt = require('jsonwebtoken');
+
+// Función para generar un token JWT válido para pruebas
+function generateAuthToken(username) {
+  return jwt.sign({ username }, 'secreto');
+}
 
 describe('GET /', () => {
   it('Deberia retornar "Hello World"', async () => {
@@ -21,8 +27,11 @@ describe('POST /api/tasks', () => {
         user_id:"1"
     };
 
+    const authToken = generateAuthToken('Javier'); // Generar un token JWT válido
+
     const response = await request(app)
       .post('/api/tasks')
+      .set('Authorization', `Bearer ${authToken}`) // Incluir el token en el encabezado de autorización
       .send(postData);
 
     expect(response.status).toBe(200);
@@ -44,8 +53,11 @@ describe('PUT /api/tasks', () => {
     
     };
 
+    const authToken = generateAuthToken('Javier'); // Generar un token JWT válido
+
     const response = await request(app)
       .put(`/api/tasks/`)
+      .set('Authorization', `Bearer ${authToken}`) // Incluir el token en el encabezado de autorización
       .send(postData);
 
     expect(response.status).toBe(200);
@@ -56,11 +68,17 @@ describe('PUT /api/tasks', () => {
 
 describe('DELETE /api/tasks/:id', () => {
   it('Deberia eliminar el task con id=3', async () => {
-    const response = await request(app).delete('/api/tasks/3');
+
+    const authToken = generateAuthToken('Javier'); // Generar un token JWT válido
+    
+    const response = await request(app)
+    .delete('/api/tasks/3')
+    .set('Authorization', `Bearer ${authToken}`); // Incluir el token en el encabezado de autorización
+    
+    
     expect(response.status).toBe(200);
     expect(response.text).toBe('3');
   });
 });
-
 
 
